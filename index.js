d4c6544ea03d6bcddbcf68c55716a8258388bf96
MehKoiter <jess54191@gmail.com>
2023-03-18 12:23:32 -0400

    Added first functionally

---
+++ index.js
@@ -0,0 +1,21 @@
+const { Client, GatewayIntentBits } = require('discord.js')
+require('dotenv/config')
+
+    const client = new Client({
+        intents: [
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.GuildMessages,
+            GatewayIntentBits.MessageContent,
+        ]
+    })
+
+    client.on('ready',()=> {
+        console.log('bot is ready')
+    } )
+    client.on('messageCreate', message => {
+        if (message.content === 'ping'){
+            message.reply('pong')
+        }
+    })
+
+    client.login(process.env.TOKEN)
