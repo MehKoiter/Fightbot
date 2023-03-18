36aa17430b1a9c7e95bb6a72784014d71f317b81
MehKoiter <jess54191@gmail.com>
2023-03-18 14:03:15 -0400

    Change reply to url

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ require('dotenv/config')
         console.log('bot is ready')
     } )
     client.on('messageCreate', message => {
-        if (message.content === '#fights'){
-            message.reply('UFC 286 is this weekend')
+        if (message.content === '/fights'){
+            message.reply('https://www.ufc.com/events')
         }
     })
 
