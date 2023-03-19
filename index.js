7464863f33fb267ffd9422d4901f25b64ac62621
MehKoiter <jess54191@gmail.com>
2023-03-19 09:41:53 -0400

    using slash command builder

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // import fetchEvents from './fetchEvents.js';
 // setting requirements for index.js to run
-// GatewauIntentBits is the language used for discord.js to know what things to monitor.
-const { Client, GatewayIntentBits } = require('discord.js')
+// GatewauIntentBits is the language used for discord.js to know what things to monitor
+const { Client, GatewayIntentBits, SlashCommandBuilder } = require('discord.js')
 require('dotenv/config')
 
     const client = new Client({
@@ -18,13 +18,20 @@ require('dotenv/config')
         console.log('bot is ready')
     } )
 // monitoring content on pages, to look for an input
-    client.on('messageCreate', message => {
-        if (message.content === '/fights'){
-            fetchEvents();
-            message.reply('https://www.ufc.com/events')
-        }
-    })
-
+//    client.on('messageCreate', message => {
+//        if (message.content === '/fights'){
+//           fetchEvents();
+//            message.reply('https://www.ufc.com/events')
+//        }
+//    })
+    module.exprots = {
+        data: new SlashCommandBuilder()
+            .setName('fight')
+            .setDescription('This weekends fight if there is one'),
+        async execute(interaction) {
+                await interaction.reply('https://www.ufc.com/events');
+        },
+    };
     client.login(process.env.TOKEN)
 
     async function fetchEvents(){
