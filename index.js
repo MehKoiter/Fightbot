1db4c0130042cb4780fe671347095c6672630c0d
MehKoiter <jess54191@gmail.com>
2023-03-19 12:26:58 -0400

    Fix

--- index.js
+++ index.js
@@ -18,6 +18,16 @@ const { token } = require('./config.json');
         ]
     })
 
+    
+    client.commands = new Collection();
+    const commandsPath = path.join(__dirname, 'commands');
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        client.commands.set(command.data.name, command);
+    }
+
 
     // creating a new collection type or list
     const eventsPath = path.join(__dirname, 'events');
