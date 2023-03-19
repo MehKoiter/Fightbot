73cb5922ac3259c3826e0f7317fc2facdb7e695a
MehKoiter <jess54191@gmail.com>
2023-03-19 17:51:53 -0400

    update for Kyle

--- commands/fight.js
+++ commands/fight.js
@@ -1,12 +1,13 @@
-const { SlashCommandBuilder } = require('discord.js');
-const { axios } = require('axios');
+const { SlashCommandBuilder, InteractionHandler } = require('discord.js');
+const { Axios } = require('axios');
+const { Cheerio } = require('cheerio');
 
     module.exports = {
         data: new SlashCommandBuilder()
             .setName('fight')
             .setDescription('This weekends fight if there is one'),
         async execute(interaction) {
-                axios.get('https://www.ufc.com/events')
+                Axios.get('https://www.ufc.com/events')
                 .then((res) => console.log(res))
                 .catch((err) => console.log(err))
 
