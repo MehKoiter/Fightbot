a1df19eca24ae27178790cddc239b4c75ecc81c8
MehKoiter <jess54191@gmail.com>
2023-03-19 11:35:06 -0400

    created separate event files for ease of use

--- fetchEvents.js
+++
@@ -1,5 +0,0 @@
-async function fetchEvents(){
-    const response = await fetch('https://www.ufc.com/events');
-    var data = await response.json();
-    console.log(data);
-}
--- index.js
+++ index.js
@@ -20,46 +20,18 @@ const { token } = require('./config.json');
 
 
     // creating a new collection type or list
-    client.commands = new Collection();
-    const commandsPath = path.join(__dirname, 'commands');
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    const eventsPath = path.join(__dirname, 'events');
+    const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-
-    // creates the path for the commandFiles
-    for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
-        // Set a new item in the Collection with the key as the command name and the value as the exported module
-        if ('data' in command && 'execute' in command) {
-            client.commands.set(command.data.name, command);
+    for (const file of eventFiles) {
+        const filePath = path.join(eventsPath, file);
+        const event = require(filePath);
+        if (event.once) {
+            client.once(event.name, (...args) => event.execute(...args));
         } else {
-            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+            client.on(event.name, (...args) => event.execute(...args));
         }
     }
 
 
-    const rest = new REST({ version: '10' }).setToken(token);
-
-
-    // setting a consol log to tell us when the bot is ready
-    client.on(Events.InteractionCreate, async interaction => {
-        if (!interaction.isChatInputCommand()) return;
-
-        const command = interaction.client.commands.get(interaction.commandName);
-
-        if (!command) {
-            console.error(`No command matching ${interaction.commandName} was found.`);
-            return;
-        }
-
-        try {
-            await command.execute(interaction);
-        } catch (error) {
-            console.error('Bad error!' + error);
-            if (interaction.replied || interaction.deferred) {
-                await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-            } else {
-                await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-            }
-        }
-    });
+    client.login(token);
