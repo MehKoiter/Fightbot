005f89c64d1f21a04abf036b0764b5850fc0fb46
MehKoiter <jess54191@gmail.com>
2023-03-25 14:31:22 -0400

    added status

--- commands/fbhelp.js
+++ commands/fbhelp.js
@@ -5,6 +5,6 @@ export default {
         .setName('fbhelp')
         .setDescription('Ask fightbot for some command help.'), 
     execute: async (interaction) => {
-        await interaction.reply('Under Construction')
+        await interaction.reply('Type / and click on the mma glove icon to see a list of commands.')
     },
 };
--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,9 @@ export default {
 	name: Events.ClientReady,
 	once: true,
 	execute: (client) => {
-		console.log(`Ready! Logged in as ${client.user.tag}`);
+		console.log(`Ready! Logged in as ${client.user.tag}`)
+
+		client.user.setActivity(`Type /fbhelp for more info`, { type: "WATCHING"})
+
 	},
 };
