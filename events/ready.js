d941d2c56b6caee79bbd5ed03702838ebed88efb
MehKoiter <jess54191@gmail.com>
2023-03-25 14:57:46 -0400

    revert

--- events/ready.js
+++ events/ready.js
@@ -4,9 +4,6 @@ export default {
 	name: Events.ClientReady,
 	once: true,
 	execute: (client) => {
-		console.log(`Ready! Logged in as ${client.user.tag}`)
-
-		client.user.setActivity(`Type /fbhelp for more info`, { type: "WATCHING"})
-
+		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
