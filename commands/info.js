a504744ed173cfe6e53931ecdc1074e9550617d1
MehKoiter <jess54191@gmail.com>
2023-03-26 12:05:54 -0400

    Changed help command to info

--- commands/info.js
+++ commands/info.js
@@ -2,7 +2,7 @@ import { SlashCommandBuilder } from "discord.js";
 
 export default {
     data: new SlashCommandBuilder()
-        .setName('fbhelp')
+        .setName('info')
         .setDescription('Ask fightbot for some command help.'), 
     execute: async (interaction) => {
         await interaction.reply('Type / and click on the mma glove icon to see a list of commands.')
